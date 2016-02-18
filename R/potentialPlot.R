f2642966bef033c4f06bfd5eb988a9fa162221be
awaananen <amy.waananen@gmail.com>
2016-02-18 11:39:51 -0600

    function for plotting pairwise and individual mating potential for one or multiple years

--- R/potentialPlot.R
+++ R/potentialPlot.R
@@ -1,101 +1,417 @@
 #### function to make mating potential plots ####
 
+##' Visualize mating potential
+##'
+##' @title graphical visualization of a mating potential object
+##' @param matPot
+##' @param potentialType
+##' @param plotType
+##' @param showDensity
+##' @param sub.ids
+##' @param N
+##' @param sample
+##' @param sub.labels
+##' @param ind.labels
+##' @param lab.cex
+##' @param main
+##' @param ...
+##' @return nothing
+##' @return optional arguments for the plot function
+##' @export
+##' @author Stuart Wagenius
+##' @seealso see generic function \code{\link{points}} for values of \code{pch}
+##' @examples
+##' pop <- simulateScene()
+##' plotMap(pop)
+##' \dontrun{plotMap(NULL)}
+##'
 potentialPlot <-   function(matPot,
-                            plotType = c('hist','net','heat'),
+                            plotType = c('hist','heat','net'),
                             showDensity = T,
                             potentialType = c('ind','pair'),
-                            # pairwise.measure = c("synchrony", "proximity", "asynchrony", "distance"),
                             sub.ids = NULL, N = 9, sample = "random",
                             sub.labels = FALSE,
-                            # sub.lines = FALSE,
-                            # ind.nn = 5, ind.measure = NULL,
                             ind.labels = TRUE,
                             lab.cex = 0.5, main = NULL, ...){
 
+  nm <- par("mar")
+  noma <- par('oma')
+  nmfrow <- par('mfrow')
+
   if(!sample %in% c("random", "all")) {warning("sample must be 'random' or 'all'")}
 
-  if(attr(matPot,'t')){
-    potential <- 'synchrony'
-  } else if(attr(matPot,'s')){
-    potential <- 'proximity'
-  } else if(attr(matPot,'c')){
-    potential <- 'compatibility'
-  }
-
-  pt <- match.arg(plotType)
+  pt <- match.arg(plotType, c('hist','net','heat'), several.ok = TRUE)
   potentialType <- match.arg(potentialType)
-  # pm <- match.arg(pairwise.measure)
-  ids <- matPot$ind$id
-  if(is.null(main) & potentialType %in% 'ind') main <- paste('average pairwise', potential)
-  if(is.null(main) & potentialType %in% 'pair') main <- paste('pairwise', potential)
-  if (is.null(sub.ids)){
-    if (sample %in% 'all'){
-      sub.ids <- ids
-    } else {
-      sub.ids <- sample(ids, N)
-    }
-  }
 
-  if (potentialType %in% 'pair'){
-    if (pt %in% 'hist'){
-      hist(matPot[[potentialType]], prob = TRUE, breaks = 15, main = main, xlab = potential)
-      if (showDensity){
-        lines(density(matPot[[potentialType]]))
-      }
+  if (is.list(matPot[[1]])) {
+    if(attr(matPot[[1]],'t')){
+      potential <- 'synchrony'
+    } else if(attr(matPot[[1]],'s')){
+      potential <- 'proximity'
+    } else if(attr(matPot[[1]],'c')){
+      potential <- 'compatibility'
+    }
+
+    if(is.null(main) & potentialType %in% 'ind') main <- paste('average pairwise', potential)
+    if(is.null(main) & potentialType %in% 'pair') main <- paste('pairwise', potential)
+
+    nr <- length(matPot)
+    nc <- length(pt)
+
+    if (potentialType %in% 'ind'){
+      par(mfrow = c(nr,1))
+      par(oma = c(2.25,2.25,2.25,2.25))
     } else {
-      subMat<- matPot$pair[which(sub.ids %in% attr(matPot$pair,'idOrder')),which(sub.ids %in% attr(matPot$pair,'idOrder'))]
-#       print(sub.ids)
-#       print(subMat)
-      if (pt %in% 'heat'){
+      par(mfrow = c(nr,nc))
+      par(mar = c(4,0.5,0.5,2.5))
+      par(oma = c(4,4.5,2.25,1.5))
+    }
+
+    ids <- matPot[[1]][['ind']][['id']]
+
+    if (is.null(sub.ids)){
+      if (sample %in% 'all'){
+        sub.ids <- ids
+      } else {
+        sub.ids <- sample(ids, N)
+      }
+    }
+
+    for (i in 1:length(matPot)){
+      poti <- matPot[[i]]
+      iids <- poti[['ind']][['id']]
+      diag(poti[['pair']]) <- 1
+      sub.iids <- poti[['ind']][which(iids %in% sub.ids), 'id']
+      subMat<- poti[['pair']][which(sub.iids %in% attr(poti[['pair']],'idOrder')),which(sub.iids %in% attr(poti[['pair']],'idOrder'))]
+
+      if (potentialType %in% 'pair'){
+        if ('hist' %in% pt){
+          hist(poti[[potentialType]], breaks = 15, prob = T, xlab = NULL, main = NULL, ylab = "")
+          mtext(names(matPot)[i],side = 2,adj = 0.5, cex = 0.75, line = 3, font = 2)
+          if (i == nr){
+            title(xlab = potential)
+          }
+          if (showDensity){
+            lines(density(poti[[potentialType]]))
+          }
+        }
+
+        if ('net' %in% pt){
+          if(length(sub.iids)< 3){
+            plot(1, type="n", axes=F, xlab="", ylab="")
+          } else {
+            subMat[upper.tri(subMat, diag = TRUE)] <- 0
+            if(ind.labels){
+              im <- poti[['ind']][which(sub.iids %in% iids), potential]
+              lab.cex <- 1 + (im - min(im))/(max(im) - min(im))
+            }
+            plotweb3(subMat, names = sub.iids, val = FALSE, legend = FALSE, length = 0,
+                     labz.size = lab.cex, ...)
+          }
+          if (! 'hist' %in% pt){
+            mtext(names(matPot)[i],side = 2,adj = 0.5, cex = 0.75, las = 1, font = 2)
+          }
+        }
+
+        if ('heat' %in% pt){
+          if(length(sub.iids) <= 2){
+            plot(1, type="n", axes=F, xlab="", ylab="")
+          } else {
+            diag(subMat) <- 1
+            subMat[upper.tri(subMat, diag = FALSE)] <- NA
+            image(x = 1:nrow(subMat),y = 1:nrow(subMat), z = subMat, axes = F, xlab = "", ylab = "", col = colorRampPalette(c('white','red'))(12))
+            legend("topleft", legend = round(seq(min(subMat, na.rm = T),max(subMat, na.rm = T),length.out = 12),digits = 2), fill = colorRampPalette(c('white','red'))(12),ncol = 4, inset = 0.025, bty = 'n')
+            axis(1, 1:ncol(subMat), labels = sub.iids, tick = 0, cex.axis = -0.2 + 1/log10(nrow(subMat)))
+            axis(4, 1:ncol(subMat), labels = sub.iids, tick = 0, cex.axis = -0.2 + 1/log10(nrow(subMat)), las = 2)
+            par(mgp = c(3,1,0))
+          }
+          if(!'hist'%in% pt &! 'net' %in% pt){
+            mtext(names(matPot)[i],side = 2,adj = 0.5, cex = 0.75, las = 1, font = 2)
+          }
+        }
+
+      } else if (potentialType %in% 'ind') {
+        par(mar = c(1,5,1,1))
+        if ('hist' %in% pt){
+          hist(poti[[potentialType]][,potential], prob = TRUE, breaks = 15, main = NULL, xlab = NULL, ylab = NULL)
+          title(ylab = 'density', outer = T, line = -1.5)
+          mtext(names(matPot)[i],side = 2,adj = 0.5, cex = 0.75, line = 5, font = 2)
+          if (i == nr){
+            par(mar = c(4,5,1,1))
+            title(xlab = potential, outer = T, line = -2.5)
+          }
+          if (showDensity){
+            lines(density(poti[[potentialType]][,potential]))
+          }
+        } else (warning("plotType for potentialType = 'ind' must be histogram"))
+      }
+    }
+  } else {
+    diag(matPot[['pair']]) <- 1
+    if(attr(matPot,'t')){
+      potential <- 'synchrony'
+    } else if(attr(matPot,'s')){
+      potential <- 'proximity'
+    } else if(attr(matPot,'c')){
+      potential <- 'compatibility'
+    }
+
+    if(is.null(main) & potentialType %in% 'ind') main <- paste('individual average', potential)
+    if(is.null(main) & potentialType %in% 'pair') main <- paste('pairwise', potential)
+
+    nc <- length(pt)
+    # print(nc)
+    par(mfrow = c(1,nc))
+    par(mar = c(4,3.5,1.5,1.5))
+    par(oma = c(2.25, 2.25, 2.25, 2.25))
+    ids <- matPot[['ind']][['id']]
+    if (is.null(sub.ids)){
+      if (sample %in% 'all'){
+        sub.ids <- ids
+      } else {
+        sub.ids <- sample(ids, N)
+      }
+    }
+
+    subMat<- matPot$pair[which(sub.ids %in% attr(matPot$pair,'idOrder')),which(sub.ids %in% attr(matPot$pair,'idOrder'))]
+
+    if ('pair' %in% potentialType){
+      if ('hist' %in% pt){
+        hist(matPot[[potentialType]], prob = TRUE, breaks = 15, main = NULL, xlab = NULL)
+        if (showDensity){
+          lines(density(matPot[[potentialType]]))
+        }
+      }
+      if ('heat' %in% pt){
         subMat[upper.tri(subMat, diag = FALSE)] <- NA
-        par(mgp = c(0.1,0.1,0.1))
-        image(x = 1:nrow(subMat),y = 1:nrow(subMat), z = subMat, axes = F, xlab = "", ylab = "", col = heat.colors(12))
-        title(main = paste('pairwise',potential, 'heatmap'))
+        image(x = 1:nrow(subMat),y = 1:nrow(subMat), z = subMat, axes = F, xlab = "", ylab = "", col = colorRampPalette(c('white','red'))(12))
+        legend("topleft", legend = round(seq(min(subMat, na.rm = T),max(subMat, na.rm = T),length.out = 12),digits = 2), fill = colorRampPalette(c('white','red'))(12),ncol = 4, inset = 0.025, bty = 'n')
         axis(1, 1:ncol(subMat), labels = sub.ids, tick = 0, cex.axis = -0.2 + 1/log10(nrow(subMat)))
         axis(4, 1:ncol(subMat), labels = sub.ids, tick = 0, cex.axis = -0.2 + 1/log10(nrow(subMat)), las = 2)
         par(mgp = c(3,1,0))
-      } else if (pt %in% 'net'){
-        subMat[lower.tri(subMat, diag = TRUE)] <- 0
+      }
+      if ('net' %in% pt){
+        subMat[upper.tri(subMat, diag = TRUE)] <- 0
         if(ind.labels){
           im <- matPot[['ind']][which(sub.ids %in% ids), potential]
           lab.cex <- 1 + (im - min(im))/(max(im) - min(im))
         }
-        plotweb3(subMat, names = sub.ids, main = main, val = FALSE, legend = FALSE, length = 0,
+        plotweb3(subMat, names = sub.ids, val = FALSE, legend = FALSE, length = 0,
                  labz.size = lab.cex, ...)
       }
-    }
-  } else if (potentialType %in% 'ind') {
-    subVals <- matPot[['ind']][which(sub.ids %in% ids), potential]
-    if (pt %in% 'hist'){
-      hist(matPot[[potentialType]][,potential], prob = TRUE, breaks = 15, main = main,xlab = potential)
-      if (showDensity){
-        lines(density(matPot[[potentialType]][,potential]))
-      }
+    } else if ('ind' %in% potentialType) {
+      subVals <- matPot[['ind']][which(sub.ids %in% ids), potential]
+      if ('hist' %in% pt){
+        hist(matPot[[potentialType]][,potential], prob = TRUE, breaks = 15, xlab = potential, main = NULL)
+        if (showDensity){
+          lines(density(matPot[[potentialType]][,potential]))
+        }
 
-    } else (warning("plotType for potentialType = 'ind' must be histogram"))
+      } else (warning("plotType for potentialType = 'ind' must be histogram"))
+    }
   }
+  title(main = main, outer = T)
+  par(mar = nm, mfrow = nmfrow, oma = noma)
 }
 
 
-# test <- simulateScene(size = 30, sAlleles = 10)
-# syncPot <- synchrony(test, method = 'overlap',synchronyType = 'all',compareToSelf = F)
+
+#
+# test1 <- simulateScene(size = 300, sAlleles = 10)
+# test2 <- simulateScene(size = 300, sAlleles = 10)
+# test3 <- simulateScene(size = 30, sAlleles = 10)
+# test4 <- simulateScene(size = 300, sAlleles = 10)
+# syncPot1 <- synchrony(test1, method = 'augs',synchronyType = 'all',compareToSelf = F)
+# syncPot2 <- synchrony(test2, method = 'augs',synchronyType = 'all',compareToSelf = F)
+# syncPot3 <- synchrony(test3, method = 'augs',synchronyType = 'all',compareToSelf = F)
+# syncPot4 <- synchrony(test4, method = 'augs',synchronyType = 'all',compareToSelf = F)
+# #
+# syncList <- list('2000' = syncPot1, '2001' = syncPot2, '2002' = syncPot1, '2003' = syncPot4)
+#
 # proxPot <- proximity(test, method = 'maxProp')
+# #
+# ## test it out:
+# potentialPlot(syncPot, potentialType = 'pair')
+potentialPlot(syncList, potentialType = 'pair')
+# potentialPlot(syncList, potentialType = 'pair', plotType = 'net')
+#
 
-## test it out:
-potentialPlot(proxPot, potentialType = 'pair', plotType = 'heat')
 
-### Add a way to view sub.ids in histogram by drawing line/label where the subset inds are in the distribution??
 
-#     if (sub.lines){
-#       arrows(x0 = subVals, x1 = subVals, y0 = 0, y1 = max(hist(matPot$pair, prob= TRUE)$density)* 0.9)
-#     }
-#     if(sub.labels){
-#       text(x = subVals*1.1, y = max(hist(matPot$pair, prob= TRUE)$density)* 0.8, labels = sub, cex = .7)
-#     }
 
-  #### '2D' potential plots ###
-  # s & t : time series of maps, options to choose temporal scale of resolution (how many days/years displayed)
-  # s & c : two panel display with map and heatmap or a network diagram showing the compatibility of a subset
-  #         connect points on map with lines if compatible
-  # t & c : two panel display with flowering schedule/network diagram/heatmap
-  #         time series of heatmaps/network diagrams, options to choose temporal scale of resolution (how many days/years displayed)
+plotweb3 <-
+  function (flowmat, names = NULL, lab.size = 1.5, add = FALSE,
+            fig.size = 1.3, main = "", sub = "", sub2 = "", log = FALSE,
+            mar = c(0.25, 0.25, 0.25, 0.25), nullflow = NULL, minflow = NULL, maxflow = NULL,
+            legend = TRUE, leg.digit = 5, leg.title = NULL, lcol = "black",
+            arr.col = "black", val = FALSE, val.digit = 5, val.size = 0.6,
+            val.col = "red", val.title = NULL, val.ncol = 1, budget = FALSE,
+            bud.digit = 5, bud.size = 0.6, bud.title = "budget", bud.ncol = 1,
+            maxarrow = 10, minarrow = 1, length = 0.1, dcirc = 1.2, bty = "o",
+            labz.size = 1.5, ...)
+  {
+    nm <- par("mar")
+    if (ncol(flowmat) != nrow(flowmat))
+      stop("flowmat has to be square")
+    components <- names
+    if (is.null(components))
+      components <- colnames(flowmat)
+    if (is.null(components))
+      components <- rownames(flowmat)
+    if (is.null(components))
+      components <- as.character(1:ncol(flowmat))
+    numcomp <- length(components)
+    if (ncol(flowmat) != numcomp)
+      stop("flowmat and names not compatible")
+    if (length(arr.col) == 1)
+      arr.col <- matrix(nrow = numcomp, ncol = numcomp, arr.col)
+    flowmatrix <- flowmat
+    if (!is.null(nullflow)) {
+      flowmatrix[flowmatrix < nullflow[1]] <- 0
+      if (length(nullflow == 2))
+        flowmatrix[flowmatrix > nullflow[2]] <- 0
+    }
+    zero <- 0
+    if (log) {
+      flowmatrix <- log10(flowmatrix + 1e-20)
+      flowmatrix[flowmatrix == -20] <- 0
+    }
+    if (is.null(maxflow))
+      maxflow <- max(flowmatrix)
+    else if (log)
+      maxflow <- log10(maxflow)
+    if (is.null(minflow))
+      minflow <- min(flowmatrix[flowmatrix != zero])
+    else if (log)
+      minflow <- log10(minflow)
+    if (!add) {
+      figlim <- c(-fig.size, fig.size)
+      marg <- par("mar")
+      if (val)
+        mar <- mar + c(0, -2, 0, 2)
+      mar <- pmax(mar, 0)
+      par(mar = mar)
+      plot(c(0, 0), type = "n", ylab = "", asp = 1, xaxt = "n",
+           yaxt = "n", frame.plot = FALSE, xlim = figlim, ylim = figlim,
+           main = main, xlab = "")
+      mtext(side = 3, line = -1, sub)
+      mtext(side = 1, adj = 0.5, text = sub2)
+    }
+    alpha0 <- pi/2
+    alpha <- alpha0 - (1:numcomp) * 2 * pi/numcomp
+    xl <- cos(alpha)
+    yl <- sin(alpha)
+    if(length(labz.size) == 1) labz.size <- rep(labz.size, numcomp)
+    for (i in 1:numcomp) {
+      if (xl[i] > 0)
+        adjustx = 0
+      if (xl[i] < 0)
+        adjustx = 1
+      if (abs(xl[i]) < 1e-04)
+        adjustx = 0.5
+      if (yl[i] > 0)
+        adjusty = 0
+      if (yl[i] < 0)
+        adjusty = 1
+      if (abs(yl[i]) < 1e-04)
+        adjusty = 0.5
+      text(xl[i], yl[i], components[i], adj = c(adjustx, adjusty),
+           cex = par("cex") * labz.size[i])
+    }
+    circle <- function(i, lwd, col) {
+      cx <- xl[i] * dcirc
+      cy <- yl[i] * dcirc
+      r <- 0.1
+      x <- c(seq(-pi, pi, by = 0.01), pi)
+      lines(cx + r * sin(x), cy + r * cos(x), lwd = lwd, col = col)
+    }
+    par(lend = 1)
+    darrow <- (maxarrow - minarrow)/(maxflow - minflow)
+    dr <- 0.02
+    xi <- xl - dr * cos(alpha)
+    yi <- yl - dr * sin(alpha)
+    iflow <- 1
+    offset <- 1
+    ltext <- NULL
+    for (i in 1:numcomp) {
+      x2 <- xi[i]
+      y2 <- yi[i]
+      for (j in 1:i) {
+        if (flowmatrix[i, j] > zero | flowmatrix[j, i] >
+            zero) {
+          Arr.col <- arr.col[i, j]
+          x1 <- xi[j]
+          y1 <- yi[j]
+          dx <- x2 - x1
+          dy <- y2 - y1
+          ifelse(i == j, fsize <- flowmatrix[i, j], fsize <- flowmatrix[i,
+                                                                        j] - flowmatrix[j, i])
+          if (fsize > 0) {
+            code <- 1
+          }
+          else {
+            code <- 2
+            Arr.col <- arr.col[j, i]
+          }
+          size <- minarrow + darrow * (abs(fsize) - minflow)
+          if (i != j)
+            arrows(x1 + dr * dx, y1 + dr * dy, x2 - dr *
+                     dx, y2 - dr * dy, length = length, code = code,
+                   lwd = size, col = Arr.col, ...)
+          if (i == j)
+            circle(i, lwd = size, col = Arr.col)
+          if (val) {
+            text(x = (x1 + x2) * 0.5, y = (y1 + y2) * 0.5,
+                 labels = iflow, offset = offset, col = val.col)
+            ltext <- c(ltext, paste(iflow, ":", format.pval(abs(fsize),
+                                                            val.digit)))
+          }
+          iflow <- iflow + 1
+        }
+      }
+    }
+    if (legend) {
+      sizeleg = par("cex") * lab.size
+      if (!log) {
+        tmax <- maxflow
+        tmin <- minflow
+        title = leg.title
+      }
+      else {
+        tmax <- 10^maxflow
+        tmin <- 10^minflow
+        title = paste("logarithmic scale", leg.title)
+      }
+      legend("bottomright", legend = c(format.pval(tmax, leg.digit),
+                                       format.pval(tmin, leg.digit)), cex = sizeleg, title = title,
+             lwd = c(maxarrow, minarrow), bty = bty)
+    }
+    if (!val & !budget)
+      return
+    if (!add) {
+      par(mar = c(0, 0, 0, 0))
+      par(new = TRUE)
+      plot(c(0, 0), type = "n", ylab = "", xaxt = "n", yaxt = "n",
+           frame.plot = FALSE, main = "", xlab = "")
+    }
+    if (val)
+      legend("topright", legend = ltext, cex = val.size, title = val.title,
+             ncol = val.ncol, bty = bty)
+    if (budget) {
+      rate <- NULL
+      for (i in 1:numcomp) rate <- c(rate, paste(components[i],
+                                                 ":", format.pval(sum(flowmat[, i]) - sum(flowmat[i,
+                                                                                                  ]), bud.digit)))
+      legend("topleft", legend = rate, cex = bud.size, title = bud.title,
+             ncol = bud.ncol, bty = bty)
+    }
+    par(mar = nm)
+  }
+
+
+#### '2D' potential plots ###
+# s & t : time series of maps, options to choose temporal scale of resolution (how many days/years displayed)
+# s & c : two panel display with map and heatmap or a network diagram showing the compatibility of a subset
+#         connect points on map with lines if compatible
+# t & c : two panel display with flowering schedule/network diagram/heatmap
+#         time series of heatmaps/network diagrams, options to choose temporal scale of resolution (how many days/years displayed)
